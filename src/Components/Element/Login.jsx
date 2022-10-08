7b6aa0390c9d0f975ff5d4c07fda7441407ae6f9
M Zubair <md.zubair143598@gmail.com>
2022-10-08 17:27:58 +0500

    console log

--- src/Components/Element/Login.jsx
+++ src/Components/Element/Login.jsx
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [Email, setUser] = useState("");
+  const [password, setPassword]=useState("");
+
+  const handleFun = (event) => {
+    event.preventDefault();
+    const data = { Email,password};
+    console.log(data);
+  };
+
   return (
-    <div className="container con1">
-      <div className="container login">
+    <div className="container login ">
+      <div className="container con1">
         <div className="row">
           <div className="col-md-12 col1 ">
-            <h1>Login form</h1>
-            <button type="submit" className="btn1">
-              LogIn
-            </button>
-            <button type="submit" className="btn2">
-            <Link className="links" to="/signup">sign up</Link>
-            </button>
-            <br />
-            <label className="lab1" htmlFor="email">Email: </label>
-            <input className="email" type="email" placeholder="abc@email.com" />
-            <br />
-            <label  className=" lab2" htmlFor="">Password: </label>
-            <input className="password" type="password" placeholder="*****" />
-            <div className="text-center">
-            <button className="btn3" type="submit">Login</button>
-            </div>
-            <p>
-              Not a member ? <Link to="/signup">sign up</Link>
-            </p>
+            <form onSubmit={handleFun}>
+              <h1>Login form</h1>
+              <button type="submit" className="btn1">
+                LogIn
+              </button>
+              <button type="submit" className="btn2">
+                <Link className="links" to="/signup">
+                  sign up
+                </Link>
+              </button>
+              <br />
+
+              <input
+                type="email"
+                className="form-control brd mt-4 p-2"
+                value={Email}
+                onChange={(e) => {
+                  setUser(e.target.value);
+                }}
+                placeholder="enter ur Email"
+              />
+              <br />
+              
+              <input type="password" className="form-control brd mt-2 p-2"
+              value={password}
+              onChange={(e)=>
+              {
+                setPassword(e.target.value)
+              }}
+              placeholder="enter ur password"
+              />
+              <div className="text-center">
+                <button
+                  type="submit"
+                  className="btm   batn mt-5 w-100 p-3 rounded-4"
+                >
+                  Login
+                </button>
+              </div>
+              <p>
+                Not a member ? <Link to="/signup">sign up</Link>
+              </p>
+            </form>
           </div>
         </div>
       </div>
--- src/Components/Element/SignUp.jsx
+++ src/Components/Element/SignUp.jsx
@@ -1,39 +1,78 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [Email, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFun = (event) => {
+    event.preventDefault();
+    const data = { name, Email, password };
+    console.log(data);
+  };
+
   return (
     <div className="container signup">
-      <div className="container login">
+      <div className="container con1">
         <div className="row">
-          <div className="col-md-12 col1 ">
-            <h1 className='h1'>SignUp form</h1>
-            <button type="submit" className="btn1">
-            <Link className='links' to="/login">LogIn</Link>
-            </button>
-            <button type="submit" className="btn2">
-              SignUP
-            </button>
-            <div className='name'>
-                <label htmlFor="name" className='namelab'>Name: </label>
-                <input className='fname' type="text" placeholder='Enter ur name' />
+          <form onSubmit={handleFun}>
+            <div className="col-md-12 col2 ">
+              <h1 className="h1">SignUp form</h1>
+              <button type="submit" className="btn1">
+                <Link className="links" to="/login">
+                  LogIn
+                </Link>
+              </button>
+              <button type="submit" className="btn2">
+                SignUP
+              </button>
+
+              <input
+                className="form-control brd mt-4 p-2 "
+                type="text"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="Enter ur name"
+              />
+
+              <input
+                type="email"
+                className="form-control brd mt-4 p-2"
+                value={Email}
+                onChange={(e) => {
+                  setUser(e.target.value);
+                }}
+                placeholder="enter ur Email"
+              />
+              <br />
+
+              <input
+                type="password"
+                className="form-control brd mt-2 p-2"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                placeholder="enter ur password"
+              />
+              <button
+                type="submit"
+                className="btm   batn mt-5 w-100 p-3 rounded-4"
+              >
+                Sign Up
+              </button>
+              <p>
+                if member ? <Link to="/login">LogIn</Link>
+              </p>
             </div>
-            <label className="lab1" htmlFor="email">Email: </label>
-            <input className="email" type="email" placeholder="abc@email.com" />
-            <br />
-            <label  className=" lab2" htmlFor="">Password: </label>
-            <input className="password" type="password" placeholder="*****" />
-            <div className="text-center">
-            <button className="btn3" type="submit">Login</button>
-            </div>
-            <p>
-              if member ? <Link to="/login">LogIn</Link>
-            </p>
-          </div>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
